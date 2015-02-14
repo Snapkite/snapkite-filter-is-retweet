3eab912a7e96e0d00827eeb66da62c2d4ef58109
Artemij Fedosejev <artemij.fedosejev@gmail.com>
2015-02-14 19:16:01 +0000

    Add filter logic

---
+++ index.js
@@ -0,0 +1,33 @@
+function isValid(tweet) {
+  // This filter doesn't block anything
+  if (CONFIG.content.with && CONFIG.content.without) {
+    return true;
+  }
+
+  // This filter blocks all tweets
+  if (!CONFIG.content.with && !CONFIG.content.without) {
+    return false;
+  }
+
+  // Show only tweets with this content
+  if (CONFIG.content.with && !CONFIG.content.without) {
+    if (tweet.text.indexOf("RT") > -1) {
+      return true;
+    }
+
+    return false;
+  }
+
+  // Show only tweets without this content
+  if (!CONFIG.content.with && CONFIG.content.without) {
+    if (tweet.text.indexOf("RT") > -1) {
+      return false;
+    }
+
+    return true;
+  }
+}
+
+module.exports = {
+  isValid: isValid
+};
